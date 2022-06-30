7977c21b3c15b9570a4d9a8de6040e479b363285
jeoninhwa3 <jeoninhwa3@naver.com>
2022-06-30 15:05:43 +0900

    0629 update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
-$(function () {
-    var swiper = new Swiper(".mySwiper", {
-        cssMode: true,
-        navigation: {
-          nextEl: ".btn_next",
-          prevEl: ".btn_prev",
-        },
-        pagination: {
-          el: ".btn_pagination",
-        },
-        mousewheel: true,
-        keyboard: true,
-      });
+
+	// main slide
+	var swiper = new Swiper(".mySwiper", {
+		// cssMode : true,
+		navigation: {
+		  nextEl: ".btn_next",
+		  prevEl: ".btn_prev",
+		},
+		pagination: {
+			el: ".btn_pagination",
+		},
+	  });
+
+$(function() {
+	newTabmenu();
 })
+
+	//   new_section tab menu
+	function newTabmenu() {
+		$('.tab_menu li').click(function(e) {
+			e.preventDefault();
+			$('.tab_menu li').remove('on');
+			$(this).addclass('on'); 
+	
+			const idx = $(this).index();
+		})
+	}
+	
